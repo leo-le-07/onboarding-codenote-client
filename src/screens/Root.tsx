9bca96168101bb657496ceb737acb72507822ed1
Leo <coder.leo.le@gmail.com>
2019-01-27 16:30:44 +0700

    Add login and create new note features

--- src/screens/Root.tsx
+++ src/screens/Root.tsx
@@ -6,7 +6,9 @@ import AuthenticatedRoute from '../components/AuthenticatedRoute';
 import Navigation from '../components/Navigation';
 import Home from './Home';
 import NoteDetail from './NoteDetail';
+import NewNote from './NewNote';
 import Signup from './Signup';
+import Login from './Login';
 import NotFound from './NotFound';
 
 const ScreensRoot = ({ childProps }) => (
@@ -15,6 +17,8 @@ const ScreensRoot = ({ childProps }) => (
     <Switch>
       <AppliedRoute exact path="/" component={Home} props={childProps} />
       <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
+      <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
+      <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} />
       <AuthenticatedRoute path="/notes/:id" exact component={NoteDetail} props={childProps} />
       <Route component={NotFound} />
     </Switch>
